a2124dfeddd2ed69386f992a74b44af95f119715
Stanislav Vladislavovich <stanislav.vladis@yandex.ru>
2024-02-26 03:13:09 +0300

    3.4.15 Aviasales App

--- src/reducers/sorting/sortingAction.js
+++ src/reducers/sorting/sortingAction.js
@@ -2,3 +2,4 @@ import { createAction } from "@reduxjs/toolkit";
 
 //payload передается под капотом - https://www.coderdoc.ru/start/34_redux/06_toolkit/6_3_action.php
 export const sorting = createAction('SET_SORTING');
+export default sorting;
